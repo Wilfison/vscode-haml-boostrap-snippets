c3857c3abab1478a8256b72cf3316f513f551233
Wilfison <wilfisonbatista@gmail.com>
2024-12-08 15:52:37 -0300

    feat: add configuration option for CSS class completion

--- src/extension.ts
+++ src/extension.ts
@@ -3,15 +3,19 @@ import * as vscode from "vscode";
 import CompletionProvider from "./completion_provider";
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.languages.registerCompletionItemProvider(
-    { language: 'haml', scheme: 'file' },
-    new CompletionProvider(),
-    '.',
-    '"',
-    "'",
-  );
+  const config = vscode.workspace.getConfiguration('hamlBootstrap');
 
-  context.subscriptions.push(disposable);
+  if (config.get('enableCssClassCompletion')) {
+    const disposable = vscode.languages.registerCompletionItemProvider(
+      { language: 'haml', scheme: 'file' },
+      new CompletionProvider(),
+      '.',
+      '"',
+      "'",
+    );
+
+    context.subscriptions.push(disposable);
+  }
 }
 
 export function deactivate() { }
